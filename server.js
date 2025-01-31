facbbf608c992012711c17dd827474c265e4a97b
ScottL182 <scott.lynch@outlook.com>
2025-01-31 16:08:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Max-Age', 2592000); // 30 days
     res.setHeader('Access-Control-Allow-Headers', 'content-type'); // Might be helpful
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups'); // Might be helpful
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp'); // Might be helpful
     
     // Read the index.html file
     fs.readFile('index.html', (err, data) => {
